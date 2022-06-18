bd40d6770873ac680b6cbecd3eadabd9d522ae70
Shane Geary <sthomas88@live.com>
2022-06-18 09:13:21 -0700

    finsihed actibity 8

--- models/places.js
+++ models/places.js
@@ -1,3 +1,13 @@
+const mongoose = require('mongoose');
+const placeSchema = new mongoose.Schema({ 
+    name: {type: String, required: true},
+    pic: String,
+    cuisines: {type: String, required: true}, 
+    city: {type: String, required: true},
+    state:{type: String, required: true},
+    founded: Number
+});
+
 module.exports = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
@@ -11,3 +21,5 @@ module.exports = [{
     cuisines: 'Coffee, Bakery',
     pic: '/images/coffee-cat.jpg'
 }]
+
+module.exports = mongoose.model('Place', placeSchema);
