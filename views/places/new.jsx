c6cfd0c5138d2e2f5630a8eb93246c53f40c58b4
Shane Geary <sthomas88@live.com>
2022-06-18 09:22:20 -0700

    finished activity 8 with bonus

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -8,19 +8,19 @@ function new_form() {
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
                     <div className ="row">
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-6 col-md-4 col-lg-3">
                     <label htmlFor="name">Place Name</label>
                     <input className="form-control" id="name" name="name" required />
                     </div>
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="pic">Place Picture</label>
                         <input className="form-control" type="url" id="pic" name="pic"/>
                     </div>
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="city">City</label>
                         <input className="form-control" id="city" name="city"/>
                     </div>
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state"/>
                     </div>
