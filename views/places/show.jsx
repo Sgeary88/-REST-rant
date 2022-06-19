3407321a821325691d8ce36ee7c92cdd3a432da4
Shane Geary <sthomas88@live.com>
2022-06-19 11:13:15 -0700

    finished validation error

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,15 +5,27 @@ function show (data) {
     return (
         <Def>
             <main>
-                <h1>{data.place.name}</h1>
-                <h2>Ratings</h2>
-                    <p>currently unrated</p>
-                <h2>Comments</h2>
-                    <p>No comments yet!</p>
-                    <a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
-                    <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-                        <button type='submit' className='btn btn-danger'>Delete</button>
-                    </form>
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <img src={data.place.pic} alt={data.place.name}/>
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                        <div className='col-sm-6'>
+                            <h3>Rating</h3>
+                        </div>
+                        <h3>Description</h3>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <h4>
+                            Comments:
+                        </h4>
+                    </div>
+                </div>
             </main>
         </Def>
     )
